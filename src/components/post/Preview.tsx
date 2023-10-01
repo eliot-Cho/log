115fbc1cd494442221dd45e7d244ea41a50598c0
eliot-liner <eliot@linercorp.com>
2023-10-02 03:56:35 +0900

    반응형에서 날짜 깨지는 현상

--- src/components/post/Preview.tsx
+++ src/components/post/Preview.tsx
@@ -5,7 +5,7 @@ export const Preview = ({ date, title, slug, series }: Blog) => {
   return (
     <Link key={slug} href={`/post/${slug}`} className="hover:opacity-60">
       <article className="flex items-center mb-8 text-xl">
-        <span className="text-sm text-gray-500">{date}</span>
+        <span className="min-w-[75px] text-sm text-gray-500">{date}</span>
         <div className="h-4 ml-3 mr-4 border border-gray-300" />
         <span className="font-medium">{title}</span>
         {series !== "none" && (
