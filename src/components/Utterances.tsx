62202a4c2ec10999347f66c7178e9668485cddbe
eliot-liner <eliot@linercorp.com>
2023-10-02 03:15:17 +0900

    utterances 댓글 여러개 생기는 현상 해결:

--- src/components/Utterances.tsx
+++ src/components/Utterances.tsx
@@ -1,19 +1,22 @@
-export const Comment = () => (
-  <section
-    className="mt-12"
-    ref={(elem) => {
-      if (!elem) {
-        return;
-      }
-      const scriptElem = document.createElement("script");
-      scriptElem.src = "https://utteranc.es/client.js";
-      scriptElem.async = true;
-      scriptElem.setAttribute("repo", "eliot-Cho/log");
-      scriptElem.setAttribute("issue-term", "title");
-      scriptElem.setAttribute("theme", "github-light");
-      scriptElem.setAttribute("label", "blog-comment");
-      scriptElem.crossOrigin = "anonymous";
-      elem.appendChild(scriptElem);
-    }}
-  />
-);
+import { useEffect, useRef } from "react";
+
+export const Comment = () => {
+  const commentsEl = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const scriptEl = document.createElement("script");
+    scriptEl.async = true;
+    scriptEl.src = "https://utteranc.es/client.js";
+    scriptEl.setAttribute("repo", "eliot-Cho/log");
+    scriptEl.setAttribute("issue-term", "pathname");
+    scriptEl.setAttribute("theme", "github-light");
+    scriptEl.setAttribute("crossorigin", "anonymous");
+    commentsEl.current?.appendChild(scriptEl);
+  }, []);
+
+  return (
+    <div>
+      <div ref={commentsEl} />
+    </div>
+  );
+};
